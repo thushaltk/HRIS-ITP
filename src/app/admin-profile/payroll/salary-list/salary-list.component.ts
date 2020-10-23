d194d86aff68777d320577efdd32581bd7ea6854
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-10-23 12:30:58 +0530

    salary report updated

--- src/app/admin-profile/payroll/salary-list/salary-list.component.ts
+++ src/app/admin-profile/payroll/salary-list/salary-list.component.ts
@@ -5,6 +5,9 @@ import { Salary } from '../../../_models/salary.model';
 import { ConfirmService } from '../../../shared/confirm.service';
 import { ToastrService } from 'ngx-toastr';
 import { SalaryService } from '../../../_services/salary.service';
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-salary-list',
@@ -18,6 +21,7 @@ export class SalaryListComponent implements OnInit {
   searchText: string;
 
   constructor(
+    private datePipe: DatePipe,
     private router: Router,
     private salaryService: SalaryService,
     private confirmService: ConfirmService,
@@ -55,4 +59,62 @@ export class SalaryListComponent implements OnInit {
         (reject) => {}
       );
   }
+
+
+  report() {
+    let netAmount = 0;
+    let data = this.salaries.map((item) => {
+      netAmount += item.amount;
+      return [
+        this.datePipe.transform(item.date, 'yyyy-MM-dd') || '-',
+        item.amountOfLeaves,
+        item.penaltyForLeaves,
+        item.otHours,
+        item.otPay,
+        item.amount,
+        netAmount,
+      ];
+    });
+    let doc = new jsPDF('l', 'mm', [305, 250]);
+    doc.text('UK Engineering Services (PVT) Ltd', 35, 10);
+    doc.text(
+      `Salary Report of ${this.payroll?.employee?.fullName} ${this.datePipe.transform(
+        new Date(),
+        'yyyy-MM-dd HH:mm'
+      )}`,
+      35,
+      20
+    );
+
+    let img = new Image();
+    img.src = 'assets/images/logo.png';
+    doc.addImage(img, 10, 5, 20, 20);
+
+    autoTable(doc, {
+      head: [
+        [
+          'Date',
+          'Amount of Leaves',
+          'Penalty for Leaves',
+          'OT Hours',
+          'OT Pay',
+          'Amount',
+        ],
+      ],
+      body: data,
+      margin: { top: 40 },
+    });
+
+    // let finalY = doc.lastAutoTable.finalY;
+    // doc.setFontSize(12);
+    // doc.text(`Net Amount Paid Rs: ${netAmount}/=`, 10, finalY + 10);
+
+    doc.save(
+      `Salary Report - ${this.payroll?.employee?.fullName} - ${this.datePipe.transform(
+        new Date(),
+        'yyyy-MM-dd HH:mm'
+      )}`
+    );
+  }
+
 }
